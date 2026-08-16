0f4e03f871227f76137cc79287ac912fd67b8e68
Amar-03creator <fiku456@gmail.com>
2026-08-16 15:52:26 +0530

    Fixed Vite dirname and AWS CORS policy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   // This tells Vite to map 'global' to 'window' so Cognito works
   define: {
     global: 'window',
   },
+  // Hides your source code in the Inspect tab
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+  }
 })
